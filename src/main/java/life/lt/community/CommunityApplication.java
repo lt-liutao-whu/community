76de423530c8ab21257f7b3ae444f65f10aff264
lt-liutao.whu <247253655@qq.com>
2022-04-06 16:03:11 +0800

    init

---
+++ src/main/java/life/lt/community/CommunityApplication.java
@@ -0,0 +1,15 @@
+package life.lt.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
